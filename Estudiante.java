190acd2b3fa6de9e41f24e510d65f8a94ef8915a
azurmontelo-ui <azur.montelo@gmail.com>
2025-10-25 12:47:06 -0600

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,19 +1,19 @@
-// Clase que representa a un estudiante
 public class Estudiante {
-    // Atributos
     String nombre;
     int edad;
 
-    // Constructor: se ejecuta al crear el objeto
+    // Constructor
     public Estudiante(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
 
-    // Método: muestra la información del estudiante
+    // Método para mostrar información del estudiante
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
     }
 }
+
+
 
